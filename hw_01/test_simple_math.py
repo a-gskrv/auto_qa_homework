bc8130da51286b4a68355751d396c342e7fa8801
a-gskrv <agskrv@gmail.com>
2026-02-08 12:58:37 +0100

    Initial commit: homework 01

---
+++ hw_01/test_simple_math.py
@@ -0,0 +1,27 @@
+import pytest
+from simple_math import SimpleMath
+
+@pytest.fixture
+def simple_math():
+    return SimpleMath()
+
+# TEST SQUARE
+def test_square_negative(simple_math):
+    assert simple_math.square(-3) == 9
+
+def test_square_positive(simple_math):
+    assert simple_math.square(2) == 4
+
+def test_square_zero(simple_math):
+    assert simple_math.square(0) == 0
+
+
+# TEST CUBE
+def test_cube_negative(simple_math):
+    assert simple_math.cube(-3) == -27
+
+def test_cube_positive(simple_math):
+    assert simple_math.cube(2) == 8
+
+def test_cube_zero(simple_math):
+    assert simple_math.cube(0) == 0
