ce5cfb4d6c2df79388abc305926912b83f28cfd2
a-gskrv <agskrv@gmail.com>
2026-03-28 12:07:56 +0100

    hw7 - done, hw6 - in process

--- hw_07/employee.py
+++ hw_07/employee.py
@@ -1,14 +1,10 @@
 import requests
 
 class EmployeeApi:
-    """Класс для взаимодействия с API компаний"""
-
     def __init__(self, url):
-        """Инициализация класса с базовым URL API"""
         self.url = url
 
     def create_employ(self, data_json):
-        """Получить список всех компаний"""
         resp = requests.post(self.url + '/employee/create', json=data_json)
         assert resp.status_code == 200, f"Ошибка: ожидался статус 200, получен {resp.status_code}"
         return resp.json()
@@ -19,39 +15,31 @@ class EmployeeApi:
         return resp.json()
 
     def get_token(self, user, password):
-        """Получить токен авторизации"""
         creds = {"username": user, "password": password}
         resp = requests.post(self.url + '/auth/login', json=creds)
         assert resp.status_code == 200, f"Ошибка: ожидался статус 200, получен {resp.status_code}"
         return resp.json()["user_token"]
 
-    def edit_employee(self, user, password, employee_id, new_last_name, new_email, new_phone, new_is_active):
-        """Изменить название и описание компании по ID"""
+    def edit_employee(
+            self,
+            user,
+            password,
+            employee_id,
+            new_last_name,
+            new_email,
+            new_phone
+    ):
+
         client_token = self.get_token(user, password)
-        # employee_change__employee_id__patch
+
         url_with_token = f"{self.url}/employee/change/{employee_id}?client_token={client_token}"
 
         employee_data = {
             "last_name": new_last_name,
-            # "email": new_email,
-            # "phone": new_phone,
-            # "is_active": new_is_active
+            "email": new_email,
+            "phone": new_phone
         }
         resp = requests.patch(url_with_token, json=employee_data)
         assert resp.status_code == 200, f"Ошибка: ожидался статус 200, получен {resp.status_code}"
         return resp.json()
 
-
-"""
-def edit_employee(self, employee_id, last_name, user, password):
-        # "Изменить название и описание компании по ID"
-        client_token = self.get_token(user, password)
-        url_with_token = f"{self.url}/employee/change/{employee_id}?client_token={client_token}"
-
-        employee_data = {
-            "last_name": last_name
-        }
-        resp = requests.patch(url_with_token, json=employee_data)
-        assert resp.status_code == 200, f"Ошибка: ожидался статус 200, получен {resp.status_code}"
-        return resp.json()
-"""
--- hw_07/test_employee.py
+++ hw_07/test_employee.py
@@ -5,47 +5,41 @@ from employee import EmployeeApi
 
 base_url = "http://5.101.50.27:8000"
 api = EmployeeApi(base_url)
+
 def test_create_employee():
     employee_json = {
             "first_name": "Albert",
             "last_name": "last_name",
-            "middle_name": "string",
-            "company_id": 29,
-            "email": "user@example.com",
+            "middle_name": "middle_name",
+            "company_id": 5,
+            "email": "albert@example.com",
             "phone": "string",
-            "birthdate": "2026-03-18",
+            "birthdate": "1984-04-17",
             "is_active": True
     }
 
-    # api = EmployeeApi(base_url)  # Инициализация API-клиента
-    # 1.
     new_emp= api.create_employ(data_json=employee_json)
 
-    # print(emp_before.get("id"))
     pprint(new_emp)
     assert new_emp.get("first_name") == "Albert"
-    # assert new_emp.get("last_name") == "last_name"
 
-    # assert
 
-    # 2. Создаём новую компанию
 
 def test_get_employee():
 
-    employee_info = api.get_employee_by_id(1)
+    employee_info = api.get_employee_by_id(2)
 
-    assert employee_info["first_name"] == "Иван"
+    assert employee_info["first_name"] == "Сергей"
 
 
 def test_edit_employee():
     employee_info = api.edit_employee(
         user="harrypotter",
         password="expelliarmus",
-        employee_id=17,
+        employee_id=2,
         new_last_name="Albert_edit",
-        new_email="Albert_edit",
-        new_phone="asdf",
-        new_is_active="true"
+        new_email="albert_edit@albert.com",
+        new_phone="asdf"
     )
 
     assert employee_info["last_name"] == "Albert_edit"
